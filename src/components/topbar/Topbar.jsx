c383160c8f49dfa42dc3cbde8af78747c2020a0b
GannaKov <zlatta2000@gmail.com>
2023-03-25 22:53:17 +0100

    Topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,48 @@
+import "./topbar.css";
+
+import {
+  BsFillPersonFill,
+  BsSearch,
+  BsFillChatLeftTextFill,
+} from "react-icons/bs";
+
+import { MdNotifications } from "react-icons/md";
+//--------------------------------------
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">GannaSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <BsSearch />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <BsFillPersonFill />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <BsFillChatLeftTextFill />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <MdNotifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
