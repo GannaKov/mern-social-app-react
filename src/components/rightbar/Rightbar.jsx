f3b017342a4e6653b17052455f3d69fcb767951c
GannaKov <zlatta2000@gmail.com>
2023-03-27 23:39:07 +0200

    rightbar jsx and css

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,53 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b> Third Frog</b> and <b>3 other friends</b> have a birhday today.
+          </span>
+        </div>
+        <img src="/assets/ad.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/frog-2.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Second Frog</span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/frog-3.png"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Third Frog</span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/frog-5.png"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Fifth Frog</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
