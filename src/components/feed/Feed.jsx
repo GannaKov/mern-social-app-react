de0804d9a661400d3f258acb4e5e0ee0efc90366
GannaKov <zlatta2000@gmail.com>
2023-05-08 22:43:12 +0200

    prep for fetch

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,15 +1,29 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
+import axios from "axios";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+// import { Posts } from "../../dummyData";
 export default function Feed() {
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(
+        `${BASE_URL}/posts/timeline/641c86a366edd65ab75b33ba`
+      );
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((post) => (
+        {/* {Posts.map((post) => (
           <Post key={post.id} post={post} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
