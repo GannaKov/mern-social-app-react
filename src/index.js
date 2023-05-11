97a3c082972295f13cae73fa3c8004baef8eada2
GannaKov <zlatta2000@gmail.com>
2023-05-11 22:13:54 +0200

    context action reducer

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import App from "./App";
 import TimeAgo from "javascript-time-ago";
 
 import en from "javascript-time-ago/locale/en.json";
+import { AuthContextProvider } from "./context/AuthContext";
 TimeAgo.addDefaultLocale(en);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/ganna-social-app">
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter basename="/ganna-social-app">
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
